05e14a627968efc3d0e535cdf469e85a1b62e2f0
Derek Harter <derek@harter.pro>
2023-05-17 12:47:46 -0500

    Commit v0.0.2, fix circleci workflow issues
    Fixing circleci workflow issues.  Added environment variable
    in circlci to authenticate twine upload of package to pypi.
    
    Added changelog tracking to project.

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [0.0.2] - 2023-05-17
+
+### Added
+
+- Project setup, build, test, publish framework
+- Added environemnt variables for circlci publish test/publish ci workflow
+
+
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A motion tracking 3D movier render'
 LONG_DESCRIPTION = 'A package that renders movies from 3D motion tracked data, for example from 3D skeleton joint tracking data'
 
@@ -17,8 +17,10 @@ setup(
     keywords='motion tracking, 3D rendering',
     classifiers= [
         "Development Status :: 3 - Alpha",
+        "Environment :: Console",
         "Intended Audience :: Developers",
-        'License :: OSI Approved :: GPL-3',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Visualization",
     ]
 )
